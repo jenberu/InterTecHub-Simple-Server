16cd3e0258d695ca4ced7a9f33578cfc693d152e
jenberu <jemberu0970@gmail.com>
2024-11-18 00:05:27 +0300

    updated

--- message/views.py
+++ message/views.py
@@ -1,5 +1,9 @@
 from django.http import JsonResponse ,HttpResponse
 
+def home(request):
+    return JsonResponse({"hobby-url": "http://127.0.0.1:8000/messages/hobby",
+                         "name-url":"http://127.0.0.1:8000/messages/name",
+                         "dream-url":"http://127.0.0.1:8000/messages/dream"})
 def my_name(request):
     return HttpResponse("Jemberu Kassie")
 def my_hobby(request):
--- simple_web_server/urls.py
+++ simple_web_server/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from message import views
 
 
 
 urlpatterns = [
+    path('',views.home),
     path('admin/', admin.site.urls),
     path('message/',include('message.urls')),#include the message urls
 ]
